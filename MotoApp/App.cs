a408ccc3697e77831e6566be50775ab0d8d12516
Krystian <krystianprzybylski@hotmail.com>
2023-01-02 13:22:36 +0100

    interfejsy 3

--- MotoApp/App.cs
+++ MotoApp/App.cs
@@ -4,11 +4,8 @@ public class App : IApp
 {
     public void Run()
     {
-<<<<<<< HEAD
+        global::System.Console.WriteLine("tekst po zmianach");
        Console.WriteLine("Jestem tutaj, interfejs dzia³a.");
-=======
-        global::System.Console.WriteLine("jestem tutaj, dzia³aja interfejsy");
->>>>>>> 2113bfb702d58b3a0f318e0f76e80ae316accdfa
-        
+
     }
 }
